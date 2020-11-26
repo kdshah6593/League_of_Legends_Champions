218772d5ca6350778b0215d74efd5b29847a9737
Kunal <kdshah6593@gmail.com>
2020-11-26 12:26:20 -0500

    created private .env file

--- lib/lol_champions/api.rb
+++ lib/lol_champions/api.rb
@@ -2,6 +2,6 @@ class LolChampions::API
 
 
     def self.get_champions
-        response = HTTParty.get("http://ddragon.leagueoflegends.com/cdn/10.24.1/data/en_US/champion.json", {headers: {"Authorization" => "Bearer #{ENV['RIOT_API-KEY']}"} })
+        response = HTTParty.get("http://ddragon.leagueoflegends.com/cdn/10.24.1/data/en_US/champion.json", {headers: {"Authorization" => "Bearer #{ENV['RIOT_API_KEY']}"} })
     end
 end
