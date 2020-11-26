ba616d14f657903480af152899412ecf12e0c735
Kunal <kdshah6593@gmail.com>
2020-11-26 12:17:25 -0500

    updated README with Riot legalise boilerplate, set up API w/HTTParty

--- lib/lol_champions/api.rb
+++ lib/lol_champions/api.rb
@@ -1,2 +1,7 @@
-class LolChampions::API 
+class LolChampions::API
+
+
+    def self.get_champions
+        response = HTTParty.get("http://ddragon.leagueoflegends.com/cdn/10.24.1/data/en_US/champion.json", {headers: {"Authorization" => "Bearer #{ENV['RIOT_API-KEY']}"} })
+    end
 end
--- lib/lol_champions_environment.rb
+++ lib/lol_champions_environment.rb
@@ -1,3 +1,7 @@
+require "pry"
+require "httparty"
+require "dotenv/load"
+
 require_relative "./lol_champions/version"
 require_relative "./lol_champions/cli"
 require_relative "./lol_champions/champions"
