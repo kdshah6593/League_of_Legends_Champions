6e18d8d15468db445b74e380b378b9464870ab58
Kunal <kdshah6593@gmail.com>
2020-12-07 13:08:38 -0500

    remove unneeded module from environment

--- lib/lol_champions_environment.rb
+++ lib/lol_champions_environment.rb
@@ -6,8 +6,3 @@ require_relative "./lol_champions/version"
 require_relative "./lol_champions/cli"
 require_relative "./lol_champions/champion"
 require_relative "./lol_champions/api"
-
-module LolChampions
-  class Error < StandardError; end
-  # Your code goes here...
-end
